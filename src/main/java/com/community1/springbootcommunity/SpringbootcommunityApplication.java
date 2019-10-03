dbcb7920c3ece5c0dc2134ab807f2981f1868be2
ffguass <1287889626gao@gmail.com>
2019-10-03 15:44:26 +0800

    init repo

---
+++ src/main/java/com/community1/springbootcommunity/SpringbootcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.community1.springbootcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootcommunityApplication.class, args);
+    }
+
+}
